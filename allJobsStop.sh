f45c2682c885ed8edbc0e71f6282f3fc4452a90f
gioulisapo <gioulisapo@gmail.com>
2015-03-30 16:22:06 +0300

    Changes to be committed: 	new file:   allJobsStop.sh 	new file:   multijob.sh 	new file:   rmsem.sh

---
+++ allJobsStop.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+var="$(./jobCommander poll running)"
+rem="Processes running in system"
+var=${var#$rem}
+i=0
+for word in $var
+do
+    if [ $((i%2)) -eq 0 ];
+	then
+    		output="./jobCommander stop $word"
+		eval $output	
+	fi
+	i=$((i+1))
+done
---
+++ multijob.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+for i in ${*}
+	do
+	while read line           
+		do           
+   		output="./jobCommander issuejob $line"
+		eval $output  		         
+		done <$i
+	done
+echo
